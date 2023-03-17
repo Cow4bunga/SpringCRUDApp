27536971c3b3d80264b0b28f82acc14ccc56e15a
Ivan L <levitskyi.1002@gmail.com>
2023-03-17 23:20:52 +0300

    CRUD app with ModelAttribute annotation

---
+++ SpringCRUDApp2/src/main/java/by/levitsky/springcourse/controllers/PeopleController.java
@@ -0,0 +1,46 @@
+package by.levitsky.springcourse.controllers;
+
+import by.levitsky.springcourse.dao.PersonDAO;
+import by.levitsky.springcourse.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()// "/people" by default
+    public String getIndex(Model model) {
+        // Get people from DAO and send data to views
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // Get one person by id from DAO and send to views
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        // using redirect
+        return "redirect:/people";
+    }
+}
---
+++ SpringCRUDApp2/src/main/java/by/levitsky/springcourse/dao/PersonDAO.java
@@ -0,0 +1,37 @@
+package by.levitsky.springcourse.dao;
+
+import by.levitsky.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int peopleCounter;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++peopleCounter, "Joe"));
+        people.add(new Person(++peopleCounter, "Bob"));
+        people.add(new Person(++peopleCounter, "Sam"));
+        people.add(new Person(++peopleCounter, "Jack"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().
+                filter(p -> p.getId() == id).
+                findAny().
+                orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++peopleCounter);
+        people.add(person);
+    }
+}
